e424b472a3ebd0ad572515320324f7397f54cd26
한승주 <98307723+han-sueng-joo@users.noreply.github.com>
2025-09-12 12:53:44 +0900

    test9

--- firmware/CameraWebServer/app_httpd.cpp
+++ firmware/CameraWebServer/app_httpd.cpp
@@ -885,6 +885,7 @@ void startCameraServer() {
     httpd_register_uri_handler(camera_httpd, &status_uri);
     httpd_register_uri_handler(camera_httpd, &capture_uri);
     httpd_register_uri_handler(camera_httpd, &bmp_uri);
+    httpd_register_uri_handler(camera_httpd, &jpg_uri); // CUSTOM: Register /jpg handler
 
     httpd_register_uri_handler(camera_httpd, &xclk_uri);
     httpd_register_uri_handler(camera_httpd, &reg_uri);
